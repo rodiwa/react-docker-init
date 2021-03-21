b5471fa2f102a6069e7a790d6531e6f44b92a42e
Ro Diwa <rodiwaa@gmail.com>
2021-03-21 22:55:42 +0530

    init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Change this text here to see if hot module reload is working.
+          This check is mostly for bind mounts issues from windows docker engine.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
